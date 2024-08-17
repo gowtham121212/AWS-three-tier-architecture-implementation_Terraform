164db063d04251005bdba0eabc0ec6649fe3b4a3
gowtham121212 <125057215+gowtham121212@users.noreply.github.com>
2024-08-17 23:34:44 +0530

    Update variables.tf

--- 3-tier-architecture/variables.tf
+++ 3-tier-architecture/variables.tf
@@ -1,3 +1,35 @@
 variable "cidr" {
   default = "10.0.0.0/16"
 }
+
+variable "public_subnet1_cidr" {
+  default = "10.0.0.0/24"
+}
+
+variable "public_subnet2_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet1_cidr" {
+  default = "10.0.2.0/24"
+}
+
+variable "private_subnet2_cidr" {
+  default = "10.0.3.0/24"
+}
+
+variable "db_subnet1_cidr" {
+  default = "10.0.4.0/24"
+}
+
+variable "db_subnet2_cidr" {
+  default = "10.0.5.0/24"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "db_instance_class" {
+  default = "db.t2.micro"
+}
